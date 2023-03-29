82ef4f1a1594397c252a1d8a990061c5b6e2a864
Eirik Haatveit <haatveit@uio.no>
2023-03-29 10:57:52 +0200

    Do not feed the file API an empty string for config file

--- containers/test/entrypoint.sh
+++ containers/test/entrypoint.sh
@@ -3,4 +3,9 @@
 CONFIG_FILE="$1"
 
 /usr/local/bin/poetry install
-exec /usr/local/bin/tsd-file-api "$CONFIG_FILE"
+
+if [ -z "$CONFIG_FILE" ]; then
+    exec /usr/local/bin/tsd-file-api
+else
+    exec /usr/local/bin/tsd-file-api "$CONFIG_FILE"
+fi
